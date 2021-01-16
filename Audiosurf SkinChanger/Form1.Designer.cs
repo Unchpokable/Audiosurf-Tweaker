903dedc7e4f1e2f3dc6d895faa22cb9b3f21e4f4
Unchpokable <unchpokable.official@gmail.com>
2021-01-17 00:53:21 +0500

    Added some controls

--- Audiosurf SkinChanger/Form1.Designer.cs
+++ Audiosurf SkinChanger/Form1.Designer.cs
@@ -29,47 +29,59 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.pathToGameTextbox = new System.Windows.Forms.TextBox();
+            this.viewPathToGameBtn = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 390);
+            this.label1.Location = new System.Drawing.Point(12, 367);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(69, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Путь к игре:";
             // 
-            // textBox1
+            // pathToGameTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 418);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 20);
-            this.textBox1.TabIndex = 1;
+            this.pathToGameTextbox.Location = new System.Drawing.Point(12, 383);
+            this.pathToGameTextbox.Name = "pathToGameTextbox";
+            this.pathToGameTextbox.Size = new System.Drawing.Size(214, 20);
+            this.pathToGameTextbox.TabIndex = 1;
             // 
-            // button1
+            // viewPathToGameBtn
             // 
-            this.button1.Location = new System.Drawing.Point(233, 416);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "View...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.viewPathToGameBtn.Location = new System.Drawing.Point(232, 381);
+            this.viewPathToGameBtn.Name = "viewPathToGameBtn";
+            this.viewPathToGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.viewPathToGameBtn.TabIndex = 2;
+            this.viewPathToGameBtn.Text = "View...";
+            this.viewPathToGameBtn.UseVisualStyleBackColor = true;
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(12, 415);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(295, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.viewPathToGameBtn);
+            this.Controls.Add(this.pathToGameTextbox);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -81,9 +93,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox pathToGameTextbox;
+        private System.Windows.Forms.Button viewPathToGameBtn;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
--- Audiosurf SkinChanger/Form1.cs
+++ Audiosurf SkinChanger/Form1.cs
@@ -16,5 +16,10 @@ namespace Audiosurf_SkinChanger
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
