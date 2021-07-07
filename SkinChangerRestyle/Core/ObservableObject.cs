1a31e359c27cb0e2050703768acf52fe8fdd0b93
Unchpokable <IAfonen02@gmail.com>
2021-07-07 22:31:36 +0500

    Started porting to WPF Graphics

---
+++ SkinChangerRestyle/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿namespace SkinChangerRestyle.Core
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ SkinChangerRestyle/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿namespace SkinChangerRestyle.Core
+{
+    using System;
+    using System.Windows.Input;
+
+    class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> exec, Func<object, bool> canExec)
+        {
+            execute = exec;
+            canExecute = canExec;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
