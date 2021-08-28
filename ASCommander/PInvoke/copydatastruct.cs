656195346a12bc8ca2bebc1a308af3fd6312875c
Unchpokable <IAfonen02@gmail.com>
2021-08-29 00:20:09 +0500

    added "StructLayout" attribute

--- ASCommander/PInvoke/copydatastruct.cs
+++ ASCommander/PInvoke/copydatastruct.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ASCommander.PInvoke
+﻿namespace ASCommander.PInvoke
 {
+    using System;
+    using System.Runtime.InteropServices;
+
     [StructLayout(LayoutKind.Sequential)]
     public struct COPYDATASTRUCT
     {
