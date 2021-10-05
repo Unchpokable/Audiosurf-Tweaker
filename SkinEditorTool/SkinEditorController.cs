152a3f46fa072ffb683f27518f648ae6a8146b7a
Igor Afonin <unc.official.secure@protonmail.com>
2021-10-05 22:46:20 +0500

    Skin editor tool migrated from legacy ASC and adapted to new version

---
+++ SkinEditorTool/SkinEditorController.cs
@@ -0,0 +1,34 @@
+﻿namespace SkinEditorTool
+{
+    using System;
+    using ChangerAPI.Skin_Creator;
+
+    public class SkinEditorController
+    {
+        public event EventHandler EditorOpened;
+        public event EventHandler EditorClosed;
+        public event EventHandler Editor_SkinExported;
+        public event EventHandler Editor_SkinRewrited;
+        public event EventHandler Editor_SkinOpened;
+        public event EventHandler Editor_SkinClosed;
+
+        private SkinCreatorForm creatorForm;
+
+        public SkinEditorController()
+        {
+            creatorForm = new SkinCreatorForm();
+        }
+
+        public void Show()
+        {
+            creatorForm.Show();
+            EditorOpened?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Close()
+        {
+            creatorForm.Close();
+            EditorClosed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
