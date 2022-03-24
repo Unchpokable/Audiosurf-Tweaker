6aaaa0bb74ac84f7e73aaa51431ee661ec57a3d3
Igor Afonin <unc.official.secure@protonmail.com>
2022-03-24 22:20:12 +0500

    SkinChanger now can interact with Audiosurf

---
+++ SkinChangerRestyle/MVVM/Model/SkinCard.cs
@@ -0,0 +1,20 @@
+﻿using ChangerAPI.Engine;
+using SkinChangerRestyle.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SkinChangerRestyle.MVVM.Model
+{
+    class SkinCard
+    {
+        public ImageSource Cover => ((Bitmap)pinnedSkin.Cover).ToImageSource();
+        public string Name { get; set; }
+
+        private AudiosurfSkinExtended pinnedSkin;
+    }
+}
--- SkinChangerRestyle/MainWindow.xaml.cs
+++ SkinChangerRestyle/MainWindow.xaml.cs
@@ -1,32 +1,54 @@
 ﻿namespace SkinChangerRestyle
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Threading;
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
     using System.Windows.Input;
+    using System.Diagnostics;
+    using SkinChangerRestyle.Core;
+    using ASCommander;
+    using ASCommander.PInvoke;
+    using System.Drawing;
     using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
-    using SkinChangerRestyle.MVVM.View;
+    using System.Runtime.InteropServices;
+    using System;
+    using System.Windows.Controls;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WndProcMessageService wndProcMessageService;
+        private bool audiosurfConnected;
+        private string asNotConnectedStatusColor = "#9c0202";
+        private string asConnectedStatusColor = "#029c07";
+        private string asWaitForRegistratingColor = "#9c9c02";
+        private Dictionary<string, string> addFeaturesChecksEnableCommandRoute = new Dictionary<string, string>()
+        {
+            {"hideRoad", "asconfig roadvisible false" },
+            {"sidewinder", "asconfig sidewinder true" },
+            {"bankcam", "asconfig usebankingcamera true" },
+            {"hidesong", "asconfig showsongname false" }
+        };
+
+        private Dictionary<string, string> addFeaturesChecksDisableCommandRoute = new Dictionary<string, string>()
+        {
+            {"hideRoad", "asconfig roadvisible true" },
+            {"sidewinder", "asconfig sidewinder false" },
+            {"bankcam", "asconfig usebankingcamera false" },
+            {"hidesong", "asconfig showsongname true" }
+        };
 
         public MainWindow()
         {
             Thread.Sleep(1000);
             InitializeComponent();
+            wndProcMessageService = new WndProcMessageService();
+            wndProcMessageService.MessageRecieved += OnMessageRecieved;
+            audiosurfConnected = false;
+            StatusLabel.Background = (SolidColorBrush)new BrushConverter().ConvertFromString(asNotConnectedStatusColor);
+            StatusLabelContent.Text = "Audiosurf Not Connected";
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -58,6 +80,59 @@
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           Process.Start(EnvironmentContainer.gamePath ?? @"C:/");
+        }
+
+        private void ShowSubForm(object sender, RoutedEventArgs e)
+        {
+            SkinInstallationDetails details = new SkinInstallationDetails();
+            details.Show();
+        }
+
+        private void ConnectAudiosurfWindow(object sender, RoutedEventArgs e)
+        {
+            var handle = WinAPI.FindWindow(null, "Audiosurf");
+            if (handle == IntPtr.Zero)
+            {
+                System.Windows.MessageBox.Show("Unable to find Audiosurf window", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            StatusLabel.Background = (SolidColorBrush)new BrushConverter().ConvertFromString(asWaitForRegistratingColor);
+            StatusLabelContent.Text = "Handled. Wait for AS approve";
+            wndProcMessageService.Handle(handle);
+            wndProcMessageService.Command(WinAPI.WM_COPYDATA, "ascommand registerlistenerwindow AsMsgHandler");
+        }
+
+        private void OnMessageRecieved(object sender, System.Windows.Forms.Message message)
+        {
+            if (message.Msg == WinAPI.WM_COPYDATA)
+            {
+                var cds = (COPYDATASTRUCT)message.GetLParam(typeof(COPYDATASTRUCT));
+                if (cds.cbData > 0)
+                {
+                    if (cds.lpData.Contains("successfullyregistered"))
+                    {
+                        StatusLabel.Background = (SolidColorBrush)new BrushConverter().ConvertFromString(asConnectedStatusColor);
+                        StatusLabelContent.Text = "Audiosurf connected";
+                    }
+                }
+            }
+        }
+
+        private void OnAddFeaturesCheckboxChecked(object sender, RoutedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            wndProcMessageService.Command(WinAPI.WM_COPYDATA, addFeaturesChecksEnableCommandRoute[checkbox.Name]);
+        }
+
+        private void OnAddFeaturesCheckboxUnchecked(object sender, RoutedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            wndProcMessageService.Command(WinAPI.WM_COPYDATA, addFeaturesChecksDisableCommandRoute[checkbox.Name]);
         }
     }
 }
