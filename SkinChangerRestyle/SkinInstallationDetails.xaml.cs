927b5cc28fadbcf74f210915d1b055c8b658431e
Igor Afonin <unc.official.secure@protonmail.com>
2022-03-24 22:19:38 +0500

    Added skin installation details form.

---
+++ SkinChangerRestyle/SkinInstallationDetails.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SkinChangerRestyle
+{
+    /// <summary>
+    /// Логика взаимодействия для SkinInstallationDetails.xaml
+    /// </summary>
+    public partial class SkinInstallationDetails : Window
+    {
+        public SkinInstallationDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
