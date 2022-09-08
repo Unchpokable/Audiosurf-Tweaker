93ae5f624e38077d0aa6d19404168d3305ea8589
Unchpokable <unchpokable.official@gmail.com>
2022-09-09 04:13:25 +0500

    Quite more WinAPI functions for something in the future

--- ASCommander/PInvoke/WinAPI.cs
+++ ASCommander/PInvoke/WinAPI.cs
@@ -7,15 +7,24 @@ namespace ASCommander.PInvoke
     {
         public const uint WM_COPYDATA = 0x4A;
         public const uint WM_ACTIVATEAPP = 0x001C;
+        public const int SW_SHOWNORMAL = 1;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, ref COPYDATASTRUCT lParam);
-
 
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
     }
 }
