bc47f01f5b1ca1c20f6b3cd1577fb050cf8a41ee
Unchpokable <unchpokable.official@gmail.com>
2023-02-05 20:17:23 +0500

    Implemented basic DLL injection logic for injecting Tweaker' overlay renderer into game

---
+++ ASCommander/DllInject/Exceptions/ProcessNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCommander.DllInject
+{
+    internal class ProcessNotFoundException : Exception
+    {
+        public ProcessNotFoundException()
+        {
+        }
+
+        public ProcessNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ProcessNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ProcessNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
