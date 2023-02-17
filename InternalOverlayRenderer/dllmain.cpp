0ddeec32ef8f24683dd28a9d5614562a08994173
Unchpokable <unchpokable.official@gmail.com>
2023-02-18 02:59:06 +0500

    Added ImGui

--- InternalOverlayRenderer/dllmain.cpp
+++ InternalOverlayRenderer/dllmain.cpp
@@ -1,5 +1,4 @@
-﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
-#include "dllmain.h"
+﻿#include "dllmain.h"
 
 endScene pEndScene;
 reset pReset;
@@ -9,18 +8,30 @@ LPD3DXFONT font = nullptr;
 HINSTANCE DllHandle = nullptr;
 PPVOID D3DVTable = nullptr;
 D3DDEVICE_CREATION_PARAMETERS ViewportParams;
+HWND HostApplicationHandle;
+HWND GameHandle;
+WNDPROC g_WndProc_o;
 
 bool OverlayVisible = true;
+bool ImguiToolboxVisible = false;
+bool ImguiInitialized = false;
+
 
 #pragma region Overlay Rectange Parameters
 
 int ovlRectLX = 50;
-int ovlRectLY = 50;
+int ovlRectLY = 500; // Default Audiosurf window geometry - 800x600. So we place info overlay at 50px offset by OX and and window height - 100px by OY axis
 
-int ovlRectWidth = 450;
+int ovlRectWidth = 450; //450x50 should be enough to display all info that we need
 int ovlRectHeight = 50;
 
 int padding = 2;
+
+#pragma endregion
+
+#pragma region ImGui UI globals
+
+int ListboxSelect(0);
 
 #pragma endregion
 
@@ -42,33 +53,49 @@ HRESULT __stdcall DetourDetachHook(PVOID* ppPointer, PVOID pDetour)
     return result;
 }
 
-HRESULT __stdcall HookedReset(LPDIRECT3DDEVICE9 pDevice, D3DPRESENT_PARAMETERS* presentParameters)
+HRESULT __stdcall HookedReset(LPDIRECT3DDEVICE9 pDevice, PD3DPRESENT_PARAMETERS presentParameters)
 {
+    ImGui_ImplDX9_InvalidateDeviceObjects();
+    HRESULT result = pReset(pDevice, presentParameters);
+    ImGui_ImplDX9_CreateDeviceObjects();
+
     pDevice->GetCreationParameters(&ViewportParams);
 
     RECT rect;
     GetWindowRect(ViewportParams.hFocusWindow, &rect);
 
-    ovlRectLY = rect.bottom - 100;
+    ovlRectLY = rect.bottom - 100; // Correct info overlay positioning after D3D Viewport Reset
+
+    ImGui::GetIO().MouseDrawCursor = true;
 
     ActualD3DDevice = pDevice;
     D3DX_CREATE_DEFAULT_OVERLAY_FONT(pDevice, font);
-    return pReset(pDevice, presentParameters);
+    ImguiInitialized = false;
+
+    return result;
 }
 
-HRESULT __stdcall  HookedEndScene(LPDIRECT3DDEVICE9 pDevice)
+HRESULT __stdcall HookedEndScene(LPDIRECT3DDEVICE9 pDevice)
 {
     if (ActualD3DDevice == nullptr)
         ActualD3DDevice = pDevice;
-
+    
     if (!OverlayVisible)
         return pEndScene(pDevice);
 
+    if (!ImguiInitialized)
+    {
+        D3DDEVICE_CREATION_PARAMETERS params;
+        pDevice->GetCreationParameters(&params);
+
+        ImGui::CreateContext();
+        ImGui_ImplWin32_Init(params.hFocusWindow);
+        ImGui_ImplDX9_Init(pDevice);
+
+        ImguiInitialized = true;
+    }
+
     auto rectx1 = ovlRectLX, rectx2 = ovlRectLX + ovlRectWidth, recty1 = ovlRectLY, recty2 = ovlRectLY + ovlRectHeight;
-
-
-    D3DRECT rectangle = { rectx1, recty1, rectx2, recty2 };
-    //pDevice->Clear(1, &rectangle, D3DCLEAR_TARGET, D3DCOLOR_ARGB(255, 0, 0, 0), 0.0f, 0); // this draws a rectangle
 
     if (!font)
         D3DX_CREATE_DEFAULT_OVERLAY_FONT(pDevice, font);
@@ -77,8 +104,59 @@ HRESULT __stdcall  HookedEndScene(LPDIRECT3DDEVICE9 pDevice)
 
     SetRect(&textRectangle, rectx1 + padding, recty1 + padding, rectx2 - padding, recty2 - padding);
 
-    font->DrawTextA(NULL, "Audiosurf Tweaker overlay v0.1\nPress Ctrl+End to Exit overlay\nInsert to change visibility", -1, &textRectangle, DT_NOCLIP | DT_LEFT, D3DCOLOR_ARGB(255,153,255,153));
+    font->DrawTextA(NULL, "Audiosurf Tweaker overlay v0.1\nPress Shift+Esc to Exit overlay\nInsert to change visibility\nShift+End toggle menu", 
+        -1, &textRectangle, DT_NOCLIP | DT_LEFT, D3DCOLOR_ARGB(255,153,255,153));
+    
+    if (ImguiToolboxVisible)
+    {
+        DrawMenu();
+    }
+    
     return pEndScene(pDevice);
+}
+
+void __forceinline DrawMenu()
+{
+    ImGui_ImplDX9_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Audiosurf Tweaker In-game menu", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+    ImGui::Spacing();
+    ImGui::Text("Viable Tweaker' texture packages:\n");
+    ImGui::Spacing();
+
+    //debug things u know
+    const char* items[] = {"Nano", "Fractal", "Point Disarray"};
+    ImGui::ListBox("", &ListboxSelect, items, (int)ARRSIZE(items), -1);
+
+    if (ImGui::Button("Install Now"))
+    {
+        std::cout << "Call Install-package command\n";
+    }
+
+    ImGui::Spacing();
+    ImGui::Text("Shift+End to toggle this menu");
+
+    ImGui::End();
+
+    ImGui::EndFrame();
+    ImGui::Render();
+    ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+}
+
+LRESULT __stdcall SendCommandToHostApplication(LPSTR commandText)
+{
+    if (HostApplicationHandle == nullptr)
+        return 0;
+
+    COPYDATASTRUCT cds{};
+    cds.dwData = 0;
+    cds.cbData = sizeof(char) * (strlen(commandText) + 1);
+    cds.lpData = commandText;
+
+    auto msgSendResult = SendMessage(HostApplicationHandle, WM_COPYDATA, (WPARAM)GetForegroundWindow(), (LPARAM)(LPVOID)&cds);
+    return msgSendResult;
 }
 
 void InitD3D9()
@@ -124,8 +202,18 @@ void InitD3D9()
     std::cout << "Original Reset at " << vTable[16] << " detoured\n";
     pDevice->Release();
     pD3D->Release();
+
+    GameHandle = FindWindowA(NULL, "Audiosurf");
+    g_WndProc_o = (WNDPROC)SetWindowLongA(GameHandle, GWL_WNDPROC, (LRESULT)WndProc);
 }
 
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    return CallWindowProc(g_WndProc_o, hWnd, msg, wParam, lParam);
+}
 
 DWORD __stdcall EjectThread(LPVOID lpParam) 
 {
@@ -154,10 +242,11 @@ DWORD WINAPI BuildOverlay(HINSTANCE hModule)
     while (true)
     {
         Sleep(150);
-        if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_END)) 
+        if (GetAsyncKeyState(VK_SHIFT) && GetAsyncKeyState(VK_ESCAPE)) 
         {
             DetourDetachHook(&(PVOID&)pEndScene, (PVOID)HookedEndScene);
             DetourDetachHook(&(PVOID&)pReset, (PVOID)HookedReset);
+            SetWindowLongA(GameHandle, GWL_WNDPROC, (LONG_PTR)g_WndProc_o);
             fclose(fp);
             FreeConsole();
             CreateThread(0, 0, EjectThread, 0, 0, 0);
@@ -168,6 +257,13 @@ DWORD WINAPI BuildOverlay(HINSTANCE hModule)
         {
             OverlayVisible = !OverlayVisible;
         }
+
+        if (GetAsyncKeyState(VK_SHIFT) && GetAsyncKeyState(VK_END))
+        {
+            ImguiToolboxVisible = !ImguiToolboxVisible;
+        }
+
+
     }
     return 0;
 }
--- InternalOverlayRenderer/dllmain.h
+++ InternalOverlayRenderer/dllmain.h
@@ -6,26 +6,42 @@
 #include <iostream>
 #include "detours.h"
 
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_dx9.h"
+#include "imgui/backends/imgui_impl_win32.h"
+
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 #pragma comment(lib, "detours.lib")
 
-#define D3DX_CREATE_DEFAULT_OVERLAY_FONT(pDevice, font) D3DXCreateFont(pDevice, 15, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Tahoma", &font);
+#define D3DX_CREATE_DEFAULT_OVERLAY_FONT(pDevice, font) D3DXCreateFont(pDevice, 15, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Tahoma", &font)
+
+#define ARRSIZE(arr) sizeof(arr)/sizeof(arr[0])
+
+constexpr LPCSTR IntallPackageCommandHeader = "tw-Install-package";
 
 typedef void** PPVOID;
+typedef D3DPRESENT_PARAMETERS* PD3DPRESENT_PARAMETERS;
+
 typedef HRESULT(__stdcall* endScene)(LPDIRECT3DDEVICE9);
-typedef HRESULT(__stdcall* reset)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
+typedef HRESULT(__stdcall* reset)(LPDIRECT3DDEVICE9, PD3DPRESENT_PARAMETERS);
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND, UINT, WPARAM, LPARAM);
 
 HRESULT __stdcall DetourAttachHook(PVOID*, PVOID);
 HRESULT __stdcall DetourDetachHook(PVOID*, PVOID);
 
 HRESULT __stdcall HookedEndScene(LPDIRECT3DDEVICE9);
-HRESULT __stdcall HookedReset(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
+HRESULT __stdcall HookedReset(LPDIRECT3DDEVICE9, PD3DPRESENT_PARAMETERS);
+
+LRESULT __stdcall SendCommandToHostApplication(LPSTR);
+LRESULT __stdcall CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 DWORD __stdcall EjectThread(LPVOID);
 DWORD WINAPI BuildOverlay(HINSTANCE);
 
 void InitD3D9();
+void __forceinline DrawMenu();
 
 #endif // !D3DTYPES_H
 
