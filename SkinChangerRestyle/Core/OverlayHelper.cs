ae150627b264cc17fd1a40641b24e0f0f3a4ad01
Unchpokable <unchpokable.official@gmail.com>
2023-02-21 18:36:01 +0500

    fixed commands sends too fast and overlay can not intercept them

--- SkinChangerRestyle/Core/OverlayHelper.cs
+++ SkinChangerRestyle/Core/OverlayHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using ASCommander;
 using SkinChangerRestyle.Core.Extensions;
@@ -9,18 +10,52 @@ namespace SkinChangerRestyle.Core
 {
     internal class OverlayHelper
     {
+        static OverlayHelper()
+        {
+            _this = new OverlayHelper();
+        }
+
+        private OverlayHelper() { }
+
+        public event EventHandler OverlayInjected;
+
+        private static OverlayHelper _this;
+        private string _defaultInjectorPath = "\\Plugins\\InjectHelper.exe";
+        private string _defaultD3DOverlayPlugin = "\\Plugins\\InternalOverlayRenderer.dll";
+
+        public static OverlayHelper Instance
+        {
+            get
+            {
+                if (_this == null)
+                    _this = new OverlayHelper();
+                return _this;
+            }
+        }
 
         public async void InjectOverlayPlugin()
         {
             string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var overlayPluginPath = currentPath + _defaultD3DOverlayPlugin;
-
-            Extensions.Extensions.Cmd($"cd /d {currentPath}\\Plugins && InjectHelper.exe {AudiosurfHandle.Instance.Handle} {overlayPluginPath}");
-
-            await Task.Delay(100);
-
             AudiosurfHandle.Instance.MessageResieved += OnPulse;
-            AudiosurfHandle.Instance.Command("tw-callback");
+
+            Extensions.Extensions.Cmd($"cd /d {currentPath}\\Plugins && InjectHelper.exe {AudiosurfHandle.Instance.GamePID} \"{overlayPluginPath}\"");
+
+            await Task.Delay(500);
+
+            await Task.Delay(50);
+            AudiosurfHandle.Instance.Command($"tw-update-listener {AudiosurfHandle.Instance.ListenerWindowCaption}");
+            await Task.Delay(200);
+            AudiosurfHandle.Instance.Command("tw-pulse");
+            AudiosurfHandle.Instance.MessageServiceInitialized += (s, e) => AudiosurfHandle.Instance.Command($"tw-update-listener {AudiosurfHandle.Instance.ListenerWindowCaption}");
+        }
+
+        public void UpdateOverlayData(string commandHeader, string content)
+        {
+            if (!SettingsProvider.IsOverlayInstanceAlive)
+                return;
+
+            AudiosurfHandle.Instance.Command(commandHeader + " " + content);
         }
 
         private void OnPulse(object sender, string content)
@@ -32,10 +67,5 @@ namespace SkinChangerRestyle.Core
                 OverlayInjected?.Invoke(this, EventArgs.Empty);
             }
         }
-
-        public event EventHandler OverlayInjected;
-
-        private string _defaultInjectorPath = "\\Plugins\\InjectHelper.exe";
-        private string _defaultD3DOverlayPlugin = "\\Plugins\\InternalOverlayRenderer.dll";
     }
 }
