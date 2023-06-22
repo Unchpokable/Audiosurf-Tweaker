e667b979ce82458bd2071f61052d469ec893ec57
Unchpokable <Unchpokable.official@gmail.com>
2023-06-22 21:26:23 +0500

    Started developing scripting library for Server swapper module

---
+++ ServerManager/Exceptions/UnresolvedSymbolException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TweakerScripts.Exceptions
+{
+    public class UnresolvedSymbolException : Exception
+    {
+        public UnresolvedSymbolException()
+        {
+        }
+
+        public UnresolvedSymbolException(string message) : base(message)
+        {
+        }
+
+        public UnresolvedSymbolException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnresolvedSymbolException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ServerManager/Script.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TweakerScripts
+{
+    public class Script
+    {
+        public Script()
+        {
+            Operations = new List<Action>();
+        }
+
+        public Script(IList<Action> operations)
+        {
+            Operations = operations;
+        }
+
+        public event EventHandler<Exception> ScriptExecutionFault;
+
+        public IList<Action> Operations { get; private set; }
+
+        public void Execute()
+        {
+            foreach (var action in Operations)
+            {
+                try
+                {
+                    action.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    ScriptExecutionFault?.Invoke(this, ex);
+                    return;
+                }
+            }
+        }
+
+        public async void ExecuteAsync()
+        {
+            await Task.Run(() => { Execute(); });
+        }
+    }
+}
