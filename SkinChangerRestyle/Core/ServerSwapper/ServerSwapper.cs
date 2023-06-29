e2bf8d82a51022bcb10f0dbe18a37e9f42180cb4
Unchpokable <Unchpokable.official@gmail.com>
2023-06-29 23:45:07 +0500

    Added ServerSwapper class

---
+++ SkinChangerRestyle/Core/ServerSwapper/ServerSwapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TweakerScripts;
+
+namespace SkinChangerRestyle.Core.ServerSwapper
+{
+    internal class ServerSwapper
+    {
+        public ServerSwapper()
+        {
+            _defines = new Dictionary<string, string>()
+            {
+                {"%AS%", Directory.GetDirectoryRoot(Directory.GetDirectoryRoot(SettingsProvider.GameTexturesPath)) }, // => AS\engine\textures -> AS\
+                {"%BACKUP_PATH%", Assembly.GetExecutingAssembly().Location + "\\Backups"},
+            };
+        }
+         
+        private Dictionary<string, string> _defines;
+    }
+}
