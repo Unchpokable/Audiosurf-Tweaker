e42e899c5ebc1ea6600eb9ea09c6f86e2752ea85
Unchpokable <Unchpokable@outlook.com>
2023-07-14 23:21:37 +0500

    things with ServerSwapper view

--- SkinChangerRestyle/MVVM/View/ServerSwapper.xaml.cs
+++ SkinChangerRestyle/MVVM/View/ServerSwapper.xaml.cs
@@ -1,23 +1,4 @@
-﻿using ICSharpCode.AvalonEdit.CodeCompletion;
-using ICSharpCode.AvalonEdit.Highlighting;
-using ICSharpCode.AvalonEdit.Highlighting.Xshd;
-using SkinChangerRestyle.MVVM.Model;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml;
+﻿using System.Windows.Controls;
 
 namespace SkinChangerRestyle.MVVM.View
 {
@@ -29,18 +10,6 @@ namespace SkinChangerRestyle.MVVM.View
         public ServerSwapper()
         {
             InitializeComponent();
-
-            IHighlightingDefinition customHightlight;
-
-            using (var stream = new FileStream("TweakerScriptsSyntax.xshd", FileMode.Open))
-            {
-                using (var reader = new XmlTextReader(stream))
-                {
-                    customHightlight = HighlightingLoader.Load(reader, HighlightingManager.Instance);
-                }
-            }
-
-            ScriptsEditor.SyntaxHighlighting = customHightlight;
         }
     }
 }
