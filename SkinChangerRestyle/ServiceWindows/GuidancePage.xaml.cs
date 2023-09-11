e134b5412c6cfd927f764ca5817e3576e0711a11
Unchpokable <Unchpokable.official@gmail.com>
2023-09-11 20:26:11 +0500

    All service window source files moved into special folder

---
+++ SkinChangerRestyle/ServiceWindows/GuidancePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace SkinChangerRestyle
+{
+    /// <summary>
+    /// Логика взаимодействия для GuidancePage.xaml
+    /// </summary>
+    public partial class GuidancePage : Window
+    {
+        public GuidancePage(string localPath)
+        {
+            InitializeComponent();
+            var absolutePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, localPath);
+
+            var absoluteUri = new Uri(absolutePath, UriKind.Absolute);
+            //ContentViewBrowser.Navigate(absoluteUri);
+            ContentViewBrowser.Source = absoluteUri;
+        }
+    }
+}
