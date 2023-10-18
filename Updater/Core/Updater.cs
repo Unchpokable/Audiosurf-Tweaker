7548d5cec1fb5f274b3b7b9b8c8db66fd591d950
Unchpokable <Unchpokable.official@gmail.com>
2023-10-18 20:58:08 +0500

    aparted archive extraction logic + initialization static method

--- Updater/Core/Updater.cs
+++ Updater/Core/Updater.cs
@@ -12,19 +12,39 @@ namespace Updater.Core
     {
         private string _targetApplicationPath;
         private ZipArchive _updateArchiveSource;
-        private string _targetAppProcId;
+        private int _targetAppProcId;
 
+        public static Updater SetUp(string targetAppPath, string updateZip, int targetAppProcId)
+        {
+            if (!Path.Exists(targetAppPath))
+                throw new ArgumentException("Target application path does not exists");
+
+            if (!Path.Exists(updateZip)) throw new ArgumentException("Update source zip path does not exists");
+
+            var archive = new ZipArchive(File.OpenRead(updateZip), ZipArchiveMode.Read);
+
+            var targetAppProc = Process.GetProcessById(targetAppProcId);
+            if (targetAppProc == null)
+                targetAppProcId = -1;
+
+            return new Updater(targetAppPath, archive, targetAppProcId);
+        }
+
+        protected Updater(string targetApp, ZipArchive updateSource, int targetAppProcId)
+        {
+            _targetApplicationPath = targetApp;
+            _updateArchiveSource = updateSource;
+            _targetAppProcId = targetAppProcId;
+        }
 
         public void UpdateBinaries()
         {
-            var files = _updateArchiveSource.Entries.Where(entry => Path.GetExtension(entry.Name).SameWith(".exe", ".dll", ".xml"));
-
-
+            ExtractArchiveParts(".exe", ".dll", ".xml");
         }
 
-        public void UpdateOthers()
+        public void UpdateConfig()
         {
-
+            ExtractArchiveParts(".cfg", "config");
         }
 
         public void Dispose()
@@ -32,10 +52,16 @@ namespace Updater.Core
             _updateArchiveSource.Dispose();
         }
 
+        private void ExtractArchiveParts(params string[] exts)
+        {
+            var filesToExtract =
+                _updateArchiveSource.Entries.Where(entry => Path.GetExtension(entry.Name).SameWith(exts));
+            ExtractArchiveForced(filesToExtract, _targetApplicationPath);
+        }
 
         private void ShutdownTargetApplication()
         {
-            if (string.IsNullOrEmpty(_targetAppProcId))
+            if (_targetAppProcId == -1)
                 return;
 
             try
@@ -55,21 +81,21 @@ namespace Updater.Core
 
         private void ExtractArchiveForced(IEnumerable<ZipArchiveEntry> entries, string destination)
         {
-                if (!Directory.Exists(destination))
-                    Directory.CreateDirectory(destination);
+            if (!Directory.Exists(destination))
+                Directory.CreateDirectory(destination);
 
-                foreach (var entry in entries)
+            foreach (var entry in entries)
+            {
+                var destinationPath = Path.GetFullPath(Path.Combine(destination, entry.FullName));
+
+                if (entry.Length == 0)
                 {
-                    var destinationPath = Path.GetFullPath(Path.Combine(destination, entry.FullName));
-
-                    if (entry.Length == 0)
-                    {
-                        Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
-                        continue;
-                    }
-
-                    entry.ExtractToFile(destinationPath, true);
+                    Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+                    continue;
                 }
+
+                entry.ExtractToFile(destinationPath, true);
+            }
             
         }
     }
