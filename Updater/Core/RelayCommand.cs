b160933b1112f9eaa6f13baaedadb3ab3bdc977e
Unchpokable <Unchpokable.official@gmail.com>
2023-10-18 20:57:37 +0500

    ICommand implementation for MVVM

---
+++ Updater/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Updater.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> exec, Func<object, bool> canExec = null)
+        {
+            _execute = exec;
+            _canExecute = canExec;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
