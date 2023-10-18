0bd8256f5fae7174b620570eeabdf5023628b8c2
Unchpokable <Unchpokable.official@gmail.com>
2023-10-18 20:57:23 +0500

    Removed strange extensions

--- Updater/Core/Extensions/Extensions.cs
+++ Updater/Core/Extensions/Extensions.cs
@@ -6,30 +6,6 @@ namespace Updater.Core.Extensions
 {
     public static class Extensions
     {
-        public static T? FirstOrNull<T>(this IEnumerable<T> source, Predicate<T> predicate)
-            where T : class
-        {
-            foreach (var item in source)
-            {
-                if (predicate(item))
-                    return item;
-            }
-
-            return null;
-        }
-
-        public static T? FirstOfNull<T>(this IEnumerable<T> source, Predicate<T> predicate) 
-            where T : struct
-        {
-            foreach (var item in source)
-            {
-                if (predicate(item))
-                    return item;
-            }
-
-            return null;
-        }
-
         public static bool SameWith<T>(this T source, params T[] templates)
             where T: IEquatable<T>
         {
