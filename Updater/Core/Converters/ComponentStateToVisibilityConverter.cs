fbbad26eb72f731f744a7f8160ff2e4661d7a4af
Unchpokable <Unchpokable.official@gmail.com>
2023-10-18 20:55:57 +0500

    Converter for binding ComponentVisibility Enum to Visibility according to selected ComponentVisibility value

---
+++ Updater/Core/Converters/ComponentStateToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using Updater.Models;
+
+namespace Updater.Core.Converters
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is ComponentVisibility visibility && parameter is ComponentVisibility targetVisibility)
+            {
+                if (visibility == targetVisibility)
+                    return Visibility.Visible;
+                else
+                    return Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
