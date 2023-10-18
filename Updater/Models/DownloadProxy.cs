1da90e2844a49933a063b517b9f36c2da93cfb1e
Unchpokable <Unchpokable.official@gmail.com>
2023-10-18 21:01:05 +0500

    Refactoring

--- Updater/Models/DownloadProxy.cs
+++ Updater/Models/DownloadProxy.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Updater.Core;
 
@@ -17,7 +14,6 @@ namespace Updater.Models
         {
             _downloadUrl = url;
         }
-
 
         public long TotalSize
         {
@@ -56,19 +52,10 @@ namespace Updater.Models
 
         private int _defaultBufferSize = 8192;
 
-        /// <summary>
-        /// Starts downloading file by given URL and returns path to downloaded file
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException">Throws if object was initialized with null url and StartDownloadAsync was called without parameter</exception>
-        public async Task<string> StartDownloadAsync(string url = null)
+        public async Task<string> StartDownloadAsync()
         {
-            if (string.IsNullOrEmpty(url) && string.IsNullOrEmpty(_downloadUrl))
-                throw new ArgumentException("Target download url was null when download was tried to start");
-
-            if (url != null)
-                _downloadUrl = url;
+            if (_downloadUrl == null)
+                throw new Exception("DownloadProxy object was initialized with null-string download url");
 
             using var httpClient = new HttpClient(new HttpClientHandler());
 
@@ -86,17 +73,11 @@ namespace Updater.Models
                 int bytesRead;
                 long totalBytesRead = 0;
 
-                var progress = new Progress<double>(percent =>
-                {
-                    DownloadedSize = totalBytesRead;
-                    DownloadSpeed = percent;
-                });
-
                 while ((bytesRead = await download.ReadAsync(buffer, 0, buffer.Length)) > 0)
                 {
                     await fileStream.WriteAsync(buffer, 0, bytesRead);
                     totalBytesRead += bytesRead;
-                    ((IProgress<double>)progress).Report((double)totalBytesRead / TotalSize);
+                    DownloadedSize = totalBytesRead;
                 }
 
                 return fileStream.Name;
